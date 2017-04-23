120ee7dc9e80afc6d8d31c1ef6f2a90336c82941
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-04-23 22:01:59 +0100

    added tests

--- test/test.js
+++ test/test.js
@@ -133,3 +133,57 @@ runner.test('sort by deep value, custom order', function () {
   const result = sortBy(fixture, 'inner.number', customOrder)
   a.deepStrictEqual(result, expected)
 })
+
+runner.test('sort nulls', function () {
+  const expected = [
+    { importance: 'speed', weight: null },
+    { importance: 'strength', weight: null },
+    { importance: 'intelligence', weight: null },
+    { importance: 'strength', weight: 'high' },
+    { importance: 'speed', weight: 'high' },
+    { importance: 'intelligence', weight: 'high' },
+    { importance: 'intelligence', weight: 'medium' },
+    { importance: 'speed', weight: 'medium' },
+    { importance: 'strength', weight: 'medium' }
+  ]
+  const fixture = [
+    { importance: 'intelligence', weight: 'medium' },
+    { importance: 'strength', weight: 'high' },
+    { importance: 'speed', weight: null },
+    { importance: 'strength', weight: null },
+    { importance: 'speed', weight: 'high' },
+    { importance: 'intelligence', weight: null },
+    { importance: 'speed', weight: 'medium' },
+    { importance: 'intelligence', weight: 'high' },
+    { importance: 'strength', weight: 'medium' }
+  ]
+  const result = sortBy(fixture, 'weight')
+  a.deepStrictEqual(result, expected)
+})
+
+runner.test('sortBy with nulls', function () {
+  const fixture = [
+    { a: 4, b: null, c: 3 },
+    { a: 4, b: 2, c: null },
+    { a: 2, b: 2, c: 3 },
+    { a: 2, b: 2, c: 2 },
+    { a: null, b: 3, c: 4 },
+    { a: null, b: null, c: 4 },
+    { a: null, b: 2, c: 4 },
+    { a: 3, b: 3, c: 3 },
+    { a: 4, b: 3, c: null }
+  ]
+  const expected = [
+    { a: null, b: null, c: 4 },
+    { a: null, b: 2, c: 4 },
+    { a: null, b: 3, c: 4 },
+    { a: 2, b: 2, c: 2 },
+    { a: 2, b: 2, c: 3 },
+    { a: 3, b: 3, c: 3 },
+    { a: 4, b: null, c: 3 },
+    { a: 4, b: 2, c: null },
+    { a: 4, b: 3, c: null }
+  ]
+  const result = sortBy(fixture, ['a', 'b', 'c'])
+  a.deepStrictEqual(result, expected)
+})
