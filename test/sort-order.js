debeae505712226296d2ca306f1bb6f3bc6974b5
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-11-23 23:08:06 +0000

    failing tests

---
+++ test/sort-order.js
@@ -0,0 +1,61 @@
+const TestRunner = require('test-runner')
+const sort = require('../')
+const a = require('assert')
+
+const runner = new TestRunner()
+
+runner.test('sortOrder: desc 1', function () {
+  const fixture = [
+    { a: 4, b: 1, c: 1 },
+    { a: 4, b: 3, c: 1 },
+    { a: 2, b: 2, c: 3 },
+    { a: 2, b: 2, c: 2 },
+    { a: 1, b: 3, c: 4 },
+    { a: 1, b: 1, c: 4 },
+    { a: 1, b: 2, c: 4 },
+    { a: 3, b: 3, c: 3 },
+    { a: 4, b: 3, c: 1 }
+  ]
+  const expected = [
+    { a: 1, b: 3, c: 4 },
+    { a: 1, b: 2, c: 4 },
+    { a: 1, b: 1, c: 4 },
+    { a: 2, b: 2, c: 2 },
+    { a: 2, b: 2, c: 3 },
+    { a: 3, b: 3, c: 3 },
+    { a: 4, b: 3, c: 1 },
+    { a: 4, b: 3, c: 1 },
+    { a: 4, b: 1, c: 1 }
+  ]
+  const by = { a: 'asc', b: 'desc', c: 'asc' }
+  const result = sort(fixture, by)
+  a.deepStrictEqual(result, expected)
+})
+
+runner.test('sortOrder: desc 2', function () {
+  const fixture = [
+    { a: 4, b: 1, c: 1 },
+    { a: 4, b: 3, c: 1 },
+    { a: 2, b: 2, c: 3 },
+    { a: 2, b: 2, c: 2 },
+    { a: 1, b: 3, c: 4 },
+    { a: 1, b: 1, c: 4 },
+    { a: 1, b: 2, c: 4 },
+    { a: 3, b: 3, c: 3 },
+    { a: 4, b: 3, c: 1 }
+  ]
+  const expected = [
+    { a: 4, b: 1, c: 1 },
+    { a: 4, b: 3, c: 1 },
+    { a: 4, b: 3, c: 1 },
+    { a: 3, b: 3, c: 3 },
+    { a: 2, b: 2, c: 3 },
+    { a: 2, b: 2, c: 2 },
+    { a: 1, b: 1, c: 4 },
+    { a: 1, b: 2, c: 4 },
+    { a: 1, b: 3, c: 4 },
+  ]
+  const by = { a: 'desc', b: 'asc', c: 'desc' }
+  const result = sort(fixture, by)
+  a.deepStrictEqual(result, expected)
+})
