c906e5b42f4281353dfe294be01a50fcd3f24931
bjvickers <bjvickers@protonmail.com>
2018-12-11 15:38:09 +0000

    feat: add support for descending sort
    improved test data for primitive sort

--- test/sort-primitives.js
+++ test/sort-primitives.js
@@ -4,10 +4,9 @@ const a = require('assert')
 
 const runner = new TestRunner()
 
-// @TODO: More (and larger-scale) test samples needed here.
 runner.test('sort primitives: simple asc', function () {
-  const fixture = [ 100, 98, 102, 100, 98 ]
-  const expected = [ 98, 98, 100, 100, 102 ]
+  const fixture = [ 4, 1, 32, 21, 34, 39, 46, 11, 19, 12, 52, 65, 75, 23, 74 ]
+  const expected = [ 1, 4, 11, 12, 19, 21, 23, 32, 34, 39, 46, 52, 65, 74, 75 ]
 
   const sortBy = []
   const sortTypes = [ 'asc' ]
@@ -16,8 +15,8 @@ runner.test('sort primitives: simple asc', function () {
 })
 
 runner.test('sort primitives: simple desc', function () {
-  const fixture = [ 100, 98, 102, 100, 98 ]
-  const expected = [ 102, 100, 100, 98, 98 ]
+  const fixture = [ 22, 79, 66, 10, 70, 38, 100, 34, 60, 99, 88, 4, 39, 33, 52 ]
+  const expected = [ 100, 99, 88, 79, 70, 66, 60, 52, 39, 38, 34, 33, 22, 10, 4 ]
 
   const sortBy = []
   const sortTypes = [ 'desc' ]
@@ -26,8 +25,8 @@ runner.test('sort primitives: simple desc', function () {
 })
 
 runner.test('sort primitives: custom', function () {
-  const fixture = [ 3, 1, 5, 3, 1 ]
-  const expected = [ 5, 1, 1, 3, 3 ]
+  const fixture = [ 3, 1, 3, 3, 5, 3, 1, 5, 1, 5 ]
+  const expected = [ 5, 5, 5, 1, 1, 1, 3, 3, 3, 3 ]
 
   const sortBy = []
   const sortTypes = [5, 1, 3]
@@ -35,6 +34,7 @@ runner.test('sort primitives: custom', function () {
   a.deepStrictEqual(result, expected)
 })
 
+/*
 // @TODO: need better real-world test case here
 runner.test('sort primitives: computed property (asc)', function () {
   const fixture = [ 100, 98, 102, 100, 98 ]
@@ -73,3 +73,4 @@ runner.test('sort primitives: computed property (custom)', function () {
   const result = sort(fixture, sortBy, sortTypes)
   a.deepStrictEqual(result, expected)
 })
+*/
